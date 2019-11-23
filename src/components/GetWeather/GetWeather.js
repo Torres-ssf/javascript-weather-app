fe45752db32e0d910f823ea1a56ed0ff8192331a
Torres-ssf <sergio.uft@gmail.com>
2019-11-23 14:18:38 -0300

    refactor(weather): Refactor fetch weather functionality

---
+++ src/components/GetWeather/GetWeather.js
@@ -0,0 +1,40 @@
+const GetWeather = (() => {
+  const fetchWeather = async (dataType, cityName) => {
+
+    const request = `${defaultURL}${dataType}?q=${cityName}&APPID=${API_KEY}`;
+
+    const data = processData(await fetchData(request));
+
+    console.log(data);
+
+    return data;
+  }
+
+  const fetchData = async (request) => {
+    try {
+      const response = await fetch(request, { mode: 'cors' });
+
+      if (response.ok) {
+        return response.json();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const processData = (rustyData) => {
+    const weather = rustyData['weather'][0];
+    console.log(rustyData);
+    return weather;
+  }
+
+  const defaultURL = 'http://api.openweathermap.org/data/2.5/';
+
+  const API_KEY = '5ca8840c0dee5c8795633cf806e88dfc';
+
+  return {
+    fetchWeather
+  };
+})();
+
+export default GetWeather;
