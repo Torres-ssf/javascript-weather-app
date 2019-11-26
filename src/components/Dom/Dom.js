52fabced9fae48fd818fb3e86f56975f229bb286
Torres-ssf <sergio.uft@gmail.com>
2019-11-26 09:29:29 -0300

    feature(Dom): add new functionalities to the dom manipulator component

--- src/components/Dom/Dom.js
+++ src/components/Dom/Dom.js
@@ -1,14 +1,17 @@
+import wUtils from '../../utilities/WeatherUtils';
+
 const DomManipul = (() => {
+  const container = document.getElementById('container');
   // const form = document.getElementById('form');
 
   // const weatherInput = document.getElementById('weather-input');
-
+  const time = document.getElementById('time');
+  const date = document.getElementById('date');
   const cityName = document.getElementById('city-name');
-  const wDescription = document.getElementById('w-description');
-  const tempCur = document.getElementById('temp');
-  // const tempMin = document.getElementById('temp-min');
-  // const tempMax = document.getElementById('temp-max');
-  
+  const descriptionTag = document.getElementById('w-description');
+  const mainIcon = document.getElementById('main-icon');
+  const tempTag = document.getElementById('temp');
+  const foreList = document.getElementById('fore-list');
 
   // const onSubmitForm = (callback) => {
   //   form.onsubmit = callback;
@@ -17,15 +20,33 @@ const DomManipul = (() => {
   // const wInputValue = () => weatherInput.value;
 
   const setWeather = (weatherObj) => {
-    const { temp, temp_min, temp_max } = weatherObj['main'];
-    const { name } = weatherObj;
-    const { description } = weatherObj['weather'];
 
-    cityName.innerText = name;
-    // tempMin.innerText = temp_min;
-    // tempMax.innerText = temp_max;
-    tempCur.innerText = temp;
-    wDescription.innerText = description;
+    time.innerText = wUtils.getFriendlyTime();
+    date.innerText = wUtils.getFriendlyDate();
+
+    const { city, weatherArr } = weatherObj;
+    const forecastList = foreList.children;
+
+    cityName.innerText = city;
+
+    weatherArr.map((e, index) => {
+      const { temp, description, dt_txt, id } = e;
+      const backGroundStyle = `${wUtils.getIconUrl(id, dt_txt)} no-repeat 50% 50%`;
+
+      if (index == 0) {
+        container.style.background = wUtils.getContGrad(id, temp, dt_txt);
+        descriptionTag.innerText = wUtils.capitalize(description);
+        mainIcon.style.background = backGroundStyle;
+        mainIcon.style.backgroundSize = '168px 168px';
+        tempTag.innerHTML = wUtils.processTemp(temp);
+      }
+      const currentItem = forecastList[index];
+
+      currentItem.children[0].innerHTML = wUtils.friendForeTime(dt_txt);
+      currentItem.children[1].style.background = backGroundStyle;
+      currentItem.children[1].style.backgroundSize = '42px 42px';
+      currentItem.children[2].innerHTML = wUtils.processTemp(temp);
+    });
   }
 
   return {
