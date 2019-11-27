a70578df276d05e0239c5e30fda635642249bab4
Torres-ssf <sergio.uft@gmail.com>
2019-11-27 09:36:15 -0300

    asset(arrow svg): add arrow svg to be use as button background

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import GetWeather from './components/GetWeather/GetWeather';
 import DomManipul from './components/Dom/Dom';
 
-const result = GetWeather.fetchWeather('forecast', 'Sweden');
+const curWeatherProm = GetWeather.fetchWeather('weather', 'California');
+const forecastProm = GetWeather.fetchWeather('forecast', 'California');
 
-result.then(data => {
-  console.log(data);
-  DomManipul.setWeather(data);
+DomManipul.toggleLoaderContainer();
+
+curWeatherProm.then((data) => {
+  // DomManipul.setWeather(data, 'weather');
+  // DomManipul.toggleLoaderContainer();
+  // DomManipul.toggleWeatherAnimation();
 });
 
-// DomManipul.onSubmitForm((e) => {
-//   e.preventDefault();
-//   console.log(DomManipul.wInputValue());
-//   const result = GetWeather.fetchWeather('weather', DomManipul.wInputValue());
-//   result.then(data => {
-//     console.log('finaly');
-//     DomManipul.setWeather(data);
-//   });
-// });
+forecastProm.then((data) => {
+  // DomManipul.setWeather(data, 'forecast');
+  // DomManipul.toggleForecastAnimation();
+})
+
+DomManipul.onSubmitForm((e) => {
+  e.preventDefault();
+  DomManipul.toggleLoaderContainer();
+  const weather = GetWeather.fetchWeather('weather', DomManipul.wInputValue());
+  const forecast = GetWeather.fetchWeather('forecast', DomManipul.wInputValue());
+
+  weather.then((data) => {
+    // DomManipul.setWeather(data, 'weather');
+    // DomManipul.toggleLoaderContainer();
+    // DomManipul.toggleWeatherAnimation();
+  });
+
+  forecast.then((data) => {
+    // DomManipul.setWeather(data, 'forecast');
+    // DomManipul.toggleForecastAnimation();
+  }).catch((e) => {
+    // DomManipul.setErrorMessage();
+    // DomManipul.toggleLoaderContainer();
+  });
+});
