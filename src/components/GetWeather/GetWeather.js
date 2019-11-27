ebc35a6dd2627570c61cc62eb2f1c0d695219915
Torres-ssf <sergio.uft@gmail.com>
2019-11-27 09:30:17 -0300

    feature(weather): add functionality to handle another type of request

--- src/components/GetWeather/GetWeather.js
+++ src/components/GetWeather/GetWeather.js
@@ -1,16 +1,71 @@
 import WeatherUtils from '../../utilities/WeatherUtils'
 
 const GetWeather = (() => {
-  const fetchWeather = async (dataType, cityName) => {
+  const defaultURL = 'http://api.openweathermap.org/data/2.5/';
 
-    const request = `${defaultURL}${dataType}?q=${cityName}&APPID=${API_KEY}&units=metric`;
+  const API_KEY = '5ca8840c0dee5c8795633cf806e88dfc';
+
+  const processData = (rustyData, requestType) => {
+    const weatherObj = {};
+
+    console.log(rustyData);
+
+    switch (requestType) {
+      case 'weather': {
+        const { name, weather, main } = rustyData;
+        const { description, id } = weather[0];
+        const { temp } = main;
+
+        weatherObj.city = name;
+        weatherObj.description = description;
+        weatherObj.id = id;
+        weatherObj.temp = temp;
+
+        break;
+      }
+      case 'forecast': {
+        const { list } = rustyData;
+
+        const forecastArr = [];
+
+        for (let i = 0; i < list.length; i += i) {
+          const item = list[i];
+          const { weather, main } = item;
+          const dtTxt = item['dt_txt'];
+          const { temp } = main;
+          const { id } = weather[0];
+
+          forecastArr.push({
+            temp,
+            dtTxt,
+            id,
+          });
+        }
+
+        weatherObj.weatherArr = forecastArr;
+        break;
+      }
+      default:
+        console.log('No default');
+    }
+
+    weatherObj.requestType = requestType;
+
+    return weatherObj;
+  };
+
+  const fetchWeather = async (requestType, cityName) => {
+
+    let request = `${defaultURL}${requestType}?q=${cityName}&APPID=${API_KEY}&units=metric`;
+
+    if (requestType === 'forecast') request += '&cnt=5';
 
     console.log(request);
 
-    const data = processData(await fetchData(request), 1);
+    const data = processData(await fetchData(request), requestType);
 
     return data;
-  }
+  };
 
   const fetchData = async (request) => {
     try {
@@ -20,57 +75,11 @@ const GetWeather = (() => {
         return response.json();
       }
     } catch (e) {
-      console.log(e);
     }
-  }
-
-  const processData = (rustyData, typeOfRequest) => {
-
-    const weatherObj = {};
-
-    console.log(rustyData);
-
-    switch (typeOfRequest) {
-      case WeatherUtils.REQUEST_TYPE_FORECAST:
-
-        const city = rustyData['city']['name'];
-
-        weatherObj['city'] = city;
-
-        const forecastArr = [];
-
-        const list = rustyData['list'];
-
-        for (let i = 0; i < 5; i++) {
-          const item = list[i];
-          const { weather, main, dt_txt } = item;
-          const { temp } = main;
-          const { id, description } = weather[0];
-
-          forecastArr.push({
-            temp,
-            dt_txt,
-            id,
-            description,
-          });
-        }
-
-        weatherObj['weatherArr'] = forecastArr;
-        break;
-
-      default:
-        console.log('estoy default');
-    }
-
-    return weatherObj;
-  }
-
-  const defaultURL = 'http://api.openweathermap.org/data/2.5/';
-
-  const API_KEY = '5ca8840c0dee5c8795633cf806e88dfc';
+  };
 
   return {
-    fetchWeather
+    fetchWeather,
   };
 })();
 
